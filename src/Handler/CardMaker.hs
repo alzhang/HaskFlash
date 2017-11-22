ed2096ccc816b50e5b5bbf850d4362db6e161ebe
rexledesma <rex.ledesma1@gmail.com>
2017-11-22 00:42:47 -0500

    added form for adding flashcards to sets

--- src/Handler/CardMaker.hs
+++ src/Handler/CardMaker.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.CardMaker where
 
 import Import
@@ -20,7 +21,39 @@ getCardMakerR flashCardSetId = do
     flashCardSet <- runDB $ get404 flashCardSetId
     (cardMakerWidget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm (flashCardForm flashCardSetId)
     defaultLayout $ do
-        $(widgetFile "sets/card-maker")
+        [whamlet|
+            <h1> Current Set: #{flashCardSetName flashCardSet}
+            <form method=post action=@{CardMakerR flashCardSetId} enctype=#{enctype}>
+                ^{cardMakerWidget}
+                <button .ui.primary.button> Save 
+        |]
 
 postCardMakerR :: FlashCardSetId -> Handler Html
-postCardMakerR flashCardSetId = error "Not yet implemented: postCardMakerR"
+postCardMakerR flashCardSetId = do
+    ((res, _), _) <- runFormPost $ renderBootstrap3 BootstrapBasicForm (flashCardForm flashCardSetId)
+    case res of 
+        FormSuccess flashCard -> do
+            _ <- runDB $ insert flashCard
+            rows <- runDB $ selectList [FlashCardParent ==. flashCardSetId] []
+            defaultLayout $ do                
+                [whamlet| 
+                    <div .ui.success.message>
+                        <div .header>FlashCard Completed
+                        <p> You've added the card to your set!
+                        <p> #{show flashCard}
+                |]
+                [whamlet|
+                    <a href=@{CardMakerR flashCardSetId}>
+                        <div .ui.primary.basic.button>
+                            Add Another
+                |]
+                [whamlet|
+                    <div .ui.labeled.button>
+                        <a href=@{HomeR} .ui.positive.button>
+                            <i .play.icon>
+                            Play
+                        <div .ui.green.basic.left.pointing.label>
+                            #{length rows} Cards
+                |]
+        -- Error Handling 
+        _ -> defaultLayout [whamlet| <h1> looks like this was an error. |]
