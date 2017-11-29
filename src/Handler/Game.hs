825bd2db03c53caba0d33e861426c783bd6234c0
Alexander Zhang <alzhang96@gmail.com>
2017-11-29 18:20:57 -0500

    working

--- src/Handler/Game.hs
+++ src/Handler/Game.hs
@@ -11,9 +11,24 @@ import Import
 
 getGameR :: FlashCardSetId -> Handler Html
 getGameR flashCardSetId = do
-    flashcards <- runDB $ selectList ([FlashCardParent ==. flashCardSetId]) []
-    defaultLayout $ do
-        [whamlet| 
-            Need to Implement lol
-        |]
-    
+    firstCard <- runDB $ do
+      ps <- selectFirst ([FlashCardParent ==. flashCardSetId]) []
+      return ps
+    case firstCard of
+      Nothing -> defaultLayout $ do
+         [whamlet|
+           fuck rex
+         |]
+      Just (Entity cardId cardLiteral) ->
+          defaultLayout $ do
+              [whamlet|
+                  <div .ui.card>
+                      <div .content>
+                          <div .header>
+                              #{flashCardFront cardLiteral}
+                          <div .description>
+                              <b>Answer</b>: #{flashCardBack cardLiteral}
+                      <div .extra.content>
+                          <span .left.floated>
+                              Hint: #{flashCardHint cardLiteral}
+              |]
