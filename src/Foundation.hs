1abb9e6ed39914ec30f80f52570ae89a7cd45357
RexLedesma <rex.ledesma@concur.com>
2017-12-01 14:57:04 -0500

    removed unnecessary route

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -150,7 +150,7 @@ instance Yesod App where
     isAuthorized (CardMakerR _) _ = return Authorized
     isAuthorized (CardsR _) _ = return Authorized
     isAuthorized (GameR _ _) _ = return Authorized
-    isAuthorized (WrongGameR _ _) _ = return Authorized      
+    -- isAuthorized (WrongGameR _ _) _ = return Authorized      
     isAuthorized ProfileR _ = isAuthenticated
 
     -- This function creates static content files in the static folder
